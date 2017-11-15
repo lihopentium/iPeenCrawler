1029069ebca674ccdff0d3b1de16b10ff7007ef7
Liho <liho.chen@pentium.network>
2017-11-15 10:44:39 +0800

    package

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(name='iPeenCrawler',
+      version='1.0',
+      description='test fork from enginebai',
+      author='enginebai',
+      author_email='liho.chen@pentium.network',
+      packages=['iPeenCrawler'],
+      install_requires=[    # 依赖列表
+        'requests',
+        'beautifulsoup4']
+     )
